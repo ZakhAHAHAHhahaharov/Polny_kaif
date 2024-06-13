700e6a2be32a58c86e3623bfc945f4ce362aaaf9
Килрил ЗахХАХахахахахвор <135274177+ZakhAHAHAHhahaharov@users.noreply.github.com>
2024-06-14 00:25:21 +0300

    Initial commit

---
+++ djangoOpenEdu/app/forms.py
@@ -0,0 +1,48 @@
+﻿""" Definition of forms. """
+
+import email
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.db import models
+from .models import Comment
+from .models import Blog
+
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Пароль'}))
+    
+""" Description of the contents of the form """
+
+class PollForm (forms.Form):
+    name = forms.CharField(label='Ваше имя', min_length=2, max_length=100)
+    city = forms.CharField(label='Ваш город', min_length=2, max_length=100)
+    email = forms.CharField(label='Ваш email', min_length=7)
+    offer = forms.CharField(label='Какой рецепт вы бы хотели предложить?', min_length=2, max_length=10000)
+    notice = forms.BooleanField(label='Получать новости сайта?', required=False)
+    
+
+class CommentForm (forms.ModelForm):
+
+    class Meta:
+
+        model = Comment # используемая модель
+
+        fields = ('text',) # требуется заполнить только поле text
+
+        labels = {'text': "Комментарий"} # метка к полю формы text
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'description', 'content', 'image',)
+        labels = {'title': "Заголовок", 'description': "Краткое содержание", 'content': "Полное содержание", 'image': "Картинка"}
